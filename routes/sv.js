2cd5226783f1c6e974fce49b8b6f240faa61b2fe
Zhaoyu Gao <gaozy@cs.umass.edu>
2020-12-23 11:08:21 -0500

    update recruitment

--- routes/sv.js
+++ routes/sv.js
@@ -5,12 +5,20 @@ var express = require('express');
 var router = express.Router();
 
 
+//const USER_CASE_STUDY_STRING_DATES = {
+//	1: "10:00am-11:00am, Nov 9, 2020 ()",
+//	2: "10:00am-11:00am, Nov 11, 2020",
+//	3: "10:00am-11:00am, Nov 13, 2020",
+//	4: "10:00am-11:00am, Nov 14, 2020",
+//	5: "10:00am-11:00am, Nov 15, 2020"
+//};
+
 const USER_CASE_STUDY_STRING_DATES = {
-	1: "10:00am-11:00am, Nov 9, 2020",
-	2: "10:00am-11:00am, Nov 11, 2020",
-	3: "10:00am-11:00am, Nov 13, 2020",
-	4: "10:00am-11:00am, Nov 14, 2020",
-	5: "10:00am-11:00am, Nov 15, 2020"
+	1: "Dec 26, 2020, 10:00am-11:00am, EST",
+	2: "Dec 26, 2020, 11:00am-12:00am, EST",
+	3: "Dec 26, 2020, 2:00am-3:00am, EST",
+	4: "Dec 26, 2020, 3:00am-4:00am, EST",
+	5: "Dec 26, 2020, 4:00am-5:00am, EST"
 };
 
 const USER_CASE_STUDY_STRING_Q1 = "1. Email address.";
@@ -21,6 +29,9 @@ const USER_CASE_STUDY_STRING_Q3 = "3. Are you over 18 years old?";
 const USER_CASE_STUDY_STRING_Q4 = "4. Please list any games you ever played before.";
 const USER_CASE_STUDY_STRING_Q4_EXPLANATION = "RPG and shooting games are more preferred";
 const USER_CASE_STUDY_STRING_Q5 = "5. Please choose your time availability to participate in our study.";
+const USER_CASE_STUDY_STRING_Q6 = "6. To be able to take this study, you must currently live in United States. Please enter the ZIP code of your current address:";
+const USER_CASE_STUDY_STRING_Q6_EXPLANATION = "Your zip code";
+const USER_CASE_STUDY_STRING_Q7 = "7. Do you have a Windows laptop/desktop with a keyboard and a mouse (NOTE: touchpad does not work for this study)?";
 
 
 const USER_CASE_STUDY_STRING_THANK_MESSAGE
@@ -39,7 +50,10 @@ router.get('/', function(req, res) {
 			q3: USER_CASE_STUDY_STRING_Q3,
 			q4: USER_CASE_STUDY_STRING_Q4,
 			q4_exp: USER_CASE_STUDY_STRING_Q4_EXPLANATION,
-			q5: USER_CASE_STUDY_STRING_Q5
+			q5: USER_CASE_STUDY_STRING_Q5,
+			q6: USER_CASE_STUDY_STRING_Q6,
+			q6_exp: USER_CASE_STUDY_STRING_Q6_EXPLANATION,
+			q7: USER_CASE_STUDY_STRING_Q7
 		});
 });
 
